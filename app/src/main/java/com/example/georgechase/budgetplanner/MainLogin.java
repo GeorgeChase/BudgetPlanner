d0191ec86a3a12871c73092fd33a870eeeaa8784
Slurnk <george.chase.ab@gmail.com>
2017-10-23 12:16:32 -0700

    I have added all the layout files and java classes for our app, now we just need to fill them with our stuff.

---
+++ app/src/main/java/com/example/georgechase/budgetplanner/MainLogin.java
@@ -0,0 +1,17 @@
+package com.example.georgechase.budgetplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GeorgeChase on 2017-10-23.
+ */
+
+
+public class MainLogin extends AppCompatActivity {
+
+     @Override
+     protected void OnCreate(Bundle savedInstanceState) {
+
+     }
+}
---
+++ app/src/main/java/com/example/georgechase/budgetplanner/Overview.java
@@ -0,0 +1,10 @@
+package com.example.georgechase.budgetplanner;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GeorgeChase on 2017-10-23.
+ */
+
+public class Overview extends AppCompatActivity {
+}
