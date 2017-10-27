5e0ac6215ca9211d06d11c9806ef094a6b40385b
Jamie <32685735+jnguyen25@users.noreply.github.com>
2017-10-27 14:54:36 -0600

    - Fixed a theme error which caused app crash on startup - Added @Override onCreate method to all activities - Started MainLogin activity and linked to other activities

--- app/src/main/java/com/example/georgechase/budgetplanner/MainLogin.java
+++ app/src/main/java/com/example/georgechase/budgetplanner/MainLogin.java
@@ -1,7 +1,9 @@
 package com.example.georgechase.budgetplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by GeorgeChase on 2017-10-23.
@@ -11,7 +13,22 @@ import android.support.v7.app.AppCompatActivity;
 public class MainLogin extends AppCompatActivity {
 
      @Override
-     protected void OnCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.main_login);
+          setTitle("Welcome!");
+     }
 
+     public void login(View view) {
+          Intent i = new Intent(MainLogin.this, Overview.class);
+
+          //verify if user and pass match the pair saved on device
+
+          startActivity(i);
+     }
+
+     public void signUp(View view) {
+          Intent i = new Intent(MainLogin.this, Register.class);
+          startActivity(i);
      }
 }
--- app/src/main/java/com/example/georgechase/budgetplanner/Register.java
+++ app/src/main/java/com/example/georgechase/budgetplanner/Register.java
@@ -1,10 +1,26 @@
 package com.example.georgechase.budgetplanner;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by GeorgeChase on 2017-10-23.
  */
 
 public class Register extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        setTitle("Registration");
+    }
+
+    public void initializeProfile(View view) {
+        Intent i = new Intent(this, FirstTime.class);
+        //handle name data, add extras to bundle/intent
+        startActivity(i);
+    }
 }
